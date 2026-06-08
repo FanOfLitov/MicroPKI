beb10d8aaf2ec92ad161c81072fdad4b9ef32a0d
Ivan <ivandolgopolov40@gmail.com>
2026-06-08 09:46:24 +0300

    notes

--- micropki/policy.py
+++ micropki/policy.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from cryptography import x509
 from cryptography.hazmat.primitives.asymmetric import rsa, ec
 import logging
--- micropki/repository.py
+++ micropki/repository.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 """
 HTTP Repository module for MicroPKI.
 
